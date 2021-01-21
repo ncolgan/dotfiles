b640acd42bdaaedcc3cd333d9fec866fe31d963c
Nick Colgan <nick.colgan@gmail.com>
2021-01-21 09:14:17 -0600

    Remove rvm and nvm

--- symlink.sh
+++ symlink.sh
@@ -8,13 +8,13 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ ! -d "$HOME/.rvm" ]; then
-  curl -sSL https://get.rvm.io | bash
-fi
-
-if [ ! -d "$HOME/.nvm" ]; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-fi
+# if [ ! -d "$HOME/.rvm" ]; then
+#   curl -sSL https://get.rvm.io | bash
+# fi
+# 
+# if [ ! -d "$HOME/.nvm" ]; then
+#   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+# fi
 
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/vimrc ~/.vimrc
